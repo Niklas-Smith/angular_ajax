523e062e389f8a1e90d62f9af3c69559420cfc63
Niklas Smith <niklas_smith@hotmail.com>
2025-07-15 00:50:02 +0200

    input api data to table and made sort on click on th

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -18,8 +18,31 @@ constructor(private ramschemaservice : RamschemaService) {
 ngOnInit() {
 this.ramschemaservice.getRamchemaInf().subscribe(data => {
 this.CourseInfo = data;
-
 })
-
 }
+
+sortCode() {
+this.ramschemaservice.getRamchemaInf().subscribe(data => {
+this.CourseInfo = data;
+this.CourseInfo.sort((a,b) => a.code > b.code ? 1 : -1)
+})
+  
+}
+sortName() {
+this.ramschemaservice.getRamchemaInf().subscribe(data => {
+this.CourseInfo = data;
+this.CourseInfo.sort((a,b) => a.coursename > b.coursename ? 1 : -1)
+})
+  
+}
+
+sortProg() {
+this.ramschemaservice.getRamchemaInf().subscribe(data => {
+this.CourseInfo = data;
+this.CourseInfo.sort((a,b) => a.progression > b.progression ? 1 : -1)
+})
+  
+}
+
+
 }
